aab33ab38ae48844abc0be5e0cc0c2fef6e686f7
Nichapa Pakngao <pakpakninini199@gmail.com>
2020-08-10 16:46:31 +0700

    102

--- MyTestProject1/Source1.cpp
+++ MyTestProject1/Source1.cpp
@@ -1,39 +1,10 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-	int n;
-	n == 1;
-	double a, x, y, s, total1, total2;
-	total1 = 0;
-	total2 = 0;
-	scanf_s("%lf", &x);
-	scanf_s("%lf", &y);
-	a = (x + y) / 2;
-	if (x > y)
-	{
-		for (;x >= y;x--)
-		{
-			printf("%.0lf\t", x);
-			total1 += x;
-			total2 += x * x;
-			x++;
-			n++;
-		}
-	}
-	else
-	{
-		for (;x <= y;x++)
-		{
-			printf("%.0lf\t", x);
-			total1 += x;
-			total2 += x * x;
-			n++;
-		}
-	}
-	s = sqrt((n * total2 - total1 * total1) / (n * (n - 1)));
-	printf("\nAverage = %.2lf", a);
-	printf("\nSD = %.2lf", s);
+	int l, w, A;
+	scanf_s("%d", &l);
+	scanf_s("%d", &w);
+	A = l * w;
+	printf("\nThis square area is %d", A);
 	return 0;
-
 }
